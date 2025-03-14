3e691bf6382bb3acc0764d0c354bcbeb011cd160
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-03-14 15:24:56 +0100

    last cleaning (hope so)

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import { StrictMode, Suspense } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Suspense fallback={<>mama</>}>
       <App />
-    </Suspense>
-    
   </StrictMode>,
 )
